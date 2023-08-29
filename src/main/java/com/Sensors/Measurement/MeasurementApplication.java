7b73efa27413d22c91bac1dcffd3ac9df2f4cb30
Moirotsos <kostas.moirotsos@gmail.com>
2023-08-29 09:33:40 +0300

    Jenkinsfile created

--- src/main/java/com/Sensors/Measurement/MeasurementApplication.java
+++ src/main/java/com/Sensors/Measurement/MeasurementApplication.java
@@ -2,12 +2,10 @@ package com.Sensors.Measurement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 @SpringBootApplication
 public class MeasurementApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MeasurementApplication.class, args);
 	}
-
 }
